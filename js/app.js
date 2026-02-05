66d10392c1b6d8c070d939ab144dd8da3e4c8499
Jeremy Martinez <42855827+Mozz96@users.noreply.github.com>
2026-02-05 16:54:01 -0600

    v1.0
    initial commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+
+
+function yenToUSD(num1){
+    console.log("The total in USD is" + " " + ((num1 / 1000) * 7));
+
+}
+
+const result = yenToUSD(140000)
+
+
+function letsJam() {
+    console.log("3");
+    console.log("2");
+    console.log("1");
+    console.log("Let's Jam! (Watch Cowboy Bebop if you haven't!)");
+
+}
+
+const tank = letsJam()
+
+const xbox = {
+  type: "Series X",
+  consoleShape: "Fridge-shaped",
+  releaseYear: 2020,
+  consoleColor: "black",
+};
+
+console.log("The Xbox" + " " + xbox.type + " " + "released in" + " " + xbox.releaseYear)
+
+
